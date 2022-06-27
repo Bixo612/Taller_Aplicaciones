e265b7c5a912d251c404abdfd2de7d459f6c4f8a
Bixo612 <vicente.vasquez612@gmail.com>
2022-06-26 21:45:27 -0400

    26-04-2022

---
+++ tkinter/code01.py
@@ -0,0 +1,20 @@
+from tkinter import Tk, Label, Button
+
+def mensaje ():
+    print ("Mensaje del boton")
+
+ventana = Tk()
+ventana.geometry("400x400")
+ventana.title("Ventana hola mundo")
+
+lbl = Label(ventana,text='Este es un [Label] tkinter')
+lbl.pack()
+
+btn = Button(ventana, text='Presiona este [Button] para mensaje',command=mensaje,fg="red",bg="blue")
+# Otras formas de pasarle armgumentos
+# btn.config(fg="red",bg="green")
+# btn["fg"] = "green"
+# btn["bg"] = "black"
+btn.pack()
+
+ventana.mainloop()
